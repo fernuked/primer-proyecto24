40f57f0e2aebb48e3fe94aae1be83b35f0956c04
fernuked <guadalupearaya50@gmail.com>
2024-05-27 10:52:39 -0300

    creamos interfaz usuario

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,9 @@
+export interface Usuario {
+    uid: string | any; 
+    // atributos del tipo "ANY" reciben vacios o indefinidos. 
+    nombres: string;
+    apellido: string; 
+    email: string; 
+    password: string; 
+
+}
