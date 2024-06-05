9a60b0aa28863c35cd39184c25901e34c99f421c
fernuked <guadalupearaya50@gmail.com>
2024-06-05 11:13:47 -0300

    sdjgj

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+
+    firebaseConfig: {
+        apiKey: "AIzaSyCeQLYCMYl-rjZ99QKgTDhNS7_AHcNY-XA",
+        authDomain: "programacion00---2024.firebaseapp.com",
+        projectId: "programacion00---2024",
+        storageBucket: "programacion00---2024.appspot.com",
+        messagingSenderId: "822536164759",
+        appId: "1:822536164759:web:a3c2b26c4801dbdc1660d2"
+      };
+      
+}
