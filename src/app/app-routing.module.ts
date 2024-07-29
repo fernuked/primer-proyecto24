3ced2e9f069ef0acafb034582a9ae256d579d334
Lean0111 <leandroperez2147@gmail.com>
2024-07-29 10:58:56 -0300

    creamos modulo admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,6 +22,10 @@ const routes: Routes = [
   {
     path:"",loadChildren:()=>import('./modules/autentificacion/autentificacion.module').then(m=>m.AutentificacionModule)
   }
+
+  {
+    path:"",loadChildren:()=>import('./modules/admin/admin.module').then(m=>m.AdminModule)
+  }
 ]
 
 @NgModule({
