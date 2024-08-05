a8480299e01a4ddd9bda56268e010479223dd9a6
luuulaaaxd <lulitajaracruz@gmail.com>
2024-08-05 10:58:45 -0300

    creacion de metodo 'agregar producto' desde table.t t creacion de modeal desde html

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Producto } from 'src/app/models/producto';
 import { CrudService } from '../../services/crud.service';
-import { FormControl, FormGroup, Validator, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-table',
@@ -10,20 +10,43 @@ import { FormControl, FormGroup, Validator, Validators } from '@angular/forms';
 })
 export class TableComponent {
   // Creamos coleccion local de productos -> la definimos como array
-coleccionProductos: Producto[] = [];
+  coleccionProductos: Producto[] = [];
 
-// definimos formulario para los productos.
-// atributos alfanumericos (string) se inicializan con comillas simples 
-// atributos numericos (number) se inicialozan con 0 (cero jejeje)
+  // definimos formulario para los productos.
+  // atributos alfanumericos (string) se inicializan con comillas simples 
+  // atributos numericos (number) se inicialozan con 0 (cero jejeje)
 
-producto = new FormGroup({
-  nombre: new FormControl('', Validators.required),
-  precio: new FormControl('', Validators.required),
-  descripccion: new FormControl('', Validators.required),
-  categoria: new FormControl('', Validators.required),
-  imagen: new FormControl('', Validators.required),
-  alt: new FormControl('', Validators.required)
-})
+  producto = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    precio: new FormControl(0, Validators.required),
+    descripccion: new FormControl('', Validators.required),
+    categoria: new FormControl('', Validators.required),
+    imagen: new FormControl('', Validators.required),
+    alt: new FormControl('', Validators.required)
+  })
 
-constructor(public servicioCrud: CrudService){}
+  constructor(public servicioCrud: CrudService) { }
+
+  ngOnInit(): void { }
+  async agregarProducto() {
+    if (this.producto.valid) {
+      let nuevoProducto: Producto = {
+        idProducto: '',
+        nombre: this.producto.value.nombre!,
+        precio: this.producto.value.precio!,
+        descripccion: this.producto.value.descripccion!,
+        categoria: this.producto.value.categoria!,
+        imagen: this.producto.value.imagen!,
+        alt: this.producto.value.alt!,
+      }
+      await this.servicioCrud.crearProducto(nuevoProducto)
+        .then(producto => {
+          alert("Ha agregado un nuevo producto con exito")
+        })
+        .catch(error => {
+          alert("Ha ocurrido un error al cargar el prodcuto. ");
+        });
+    };
+
+  }
 }
